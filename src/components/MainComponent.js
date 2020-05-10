5a1f86c798732182192d85df4e074fb752947c56
Eugene Stepanenko <stepanenko.yevheniy@gmail.com>
2020-05-10 13:11:59 -0700

    Controlled form validation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import DishDetail from "./DishdetailComponent";
 import Header from "./HeaderComponent";
 import Contact from "./ContactComponent";
 import Footer from "./FooterComponent";
+import About from "./AboutComponent";
 import { DISHES } from "../shared/dishes";
 import { COMMENTS } from "../shared/comments";
 import { PROMOTIONS } from "../shared/promotions";
@@ -78,6 +79,11 @@ class Main extends Component {
           />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route
+            exact
+            path="/aboutus"
+            component={() => <About leaders={this.state.leaders} />}
+          />
           <Redirect to="/home" />
         </Switch>
         <Footer />
